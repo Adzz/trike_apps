5dc79dd0e3fda6cd399fdaf2954ebd0b8da9e359
Adam Lancaster <lancaster28@hotmail.com>
2017-04-26 23:48:11 +0100

    adds each trait correctly

--- lib/big_five_results_text_serializer.rb
+++ lib/big_five_results_text_serializer.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 class BigFiveResultsTextSerializer
   def initialize(text)
     @text = text
@@ -7,6 +9,7 @@ class BigFiveResultsTextSerializer
     {
       NAME: name,
       EMAIL: email,
+      EXTRAVERSION: rest[0]
     }
   end
 
@@ -14,11 +17,25 @@ class BigFiveResultsTextSerializer
 
   attr_reader :text
 
+  def traits
+    text.split(/\n\n/).drop(1)
+  end
+
   def name
-    "Jeff"
+    text.split(/\n/)[0].split(' ')[1..-1].join(' ')
   end
 
   def email
-    "lancaster28@hotmail.com"
+    text.split(/\n/)[1].split(' ').last
+  end
+
+  def rest
+    traits.map do |trait|
+      trait.split(/\n/).map.with_object({}) do |trait_string, hash|
+        hash[trait_string.delete("^a-zA-Z|\s")] = trait_string.delete("^0-9").to_i
+      end
+    end
   end
 end
+
+# probably abstract out the delimiting logic somewhere?
--- spec/big_five_results_text_serializer_spec.rb
+++ spec/big_five_results_text_serializer_spec.rb
@@ -1,15 +1,31 @@
 require_relative '../lib/big_five_results_text_serializer.rb'
 
 RSpec.describe BigFiveResultsTextSerializer do
-  let(:text) { File.read('spec/fixtures/text.txt') }
+  let(:text) { File.read('spec/fixtures/input_text.txt') }
 
   subject { described_class.new(text) }
 
-  it 'serializes the name' do
-    expect(subject.serialize[:NAME]).to eq "Jeff"
+  it 'serializes a given name' do
+    expect(subject.serialize[:NAME]).to eq "Huge Jackman"
   end
 
-  it 'serializes the email' do
+  it 'serializes the email from the input text' do
     expect(subject.serialize[:EMAIL]).to eq "lancaster28@hotmail.com"
+  end
+
+  context 'EXTRAVERSION' do
+    let(:extraversion) { subject.serialize[:EXTRAVERSION] }
+
+    it 'serializes overall score' do
+      expect(extraversion['Overall score']).to eq 68
+    end
+
+    it 'serializes Friendliness' do
+      expect(extraversion['Friendliness']).to eq 82
+    end
+
+    it 'serializes Gregariousness' do
+      expect(extraversion['Gregariousness']).to eq 80
+    end
   end
 end
